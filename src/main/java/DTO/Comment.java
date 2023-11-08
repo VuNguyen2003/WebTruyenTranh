1f6a698ec8f2cd49350ea9207156dbe4fb7be07f
EdgynCode <117133793+EdgynCode@users.noreply.github.com>
2023-11-08 13:57:09 +0700

    Update Comment.java

--- src/main/java/DTO/Comment.java
+++ src/main/java/DTO/Comment.java
@@ -1,5 +1,41 @@
 package DTO;
 
-public class Comment {
+import java.io.Serializable;
 
+@SuppressWarnings("serial")
+public class Comment implements Serializable {
+	private int userId;
+	private int storyId;
+	private String comment;
+	
+	public Comment() { }
+	public Comment(int userId, int storyId, String comment) {
+        this.userId = userId;
+        this.storyId = storyId;
+        this.comment = comment;
+    }
+	
+	public int getUserID() {
+        return userId;
+    }
+
+	public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+    public int getUserId() {
+        return storyId;
+    }
+    
+    public void setStoryID(int storyId) {
+        this.storyId = storyId;
+    }
+    
+    public String getComment() {
+    	return comment;
+    }
+    
+    public void setComment(String comment) {
+    	this.comment = comment;
+    }
 }
