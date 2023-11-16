21bca6694699566419e0006be78de2cb3b29700f
Vũ Nguyễn <116508377+VuNguyen2003@users.noreply.github.com>
2023-11-16 19:55:59 +0700

    commit

--- src/main/java/controller/uploadController.java
+++ src/main/java/controller/uploadController.java
@@ -95,6 +95,7 @@ public class uploadController extends HttpServlet {
         }
         
         System.out.println("Upload success!");
+        request.getRequestDispatcher("home").forward(request, response);
     }
 
 }
