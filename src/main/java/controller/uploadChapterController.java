e1d8a68588701849273fd9ce4a43498597554f68
Vũ Nguyễn <116508377+VuNguyen2003@users.noreply.github.com>
2023-11-20 22:29:24 +0700

    fix conflict

--- src/main/java/controller/uploadChapterController.java
+++ src/main/java/controller/uploadChapterController.java
@@ -69,10 +69,12 @@ public class uploadChapterController extends HttpServlet {
 		chapter.setStoryId(storyId);
 		chapter.setChapterName(chapterName);
 		
+		
 		page.setPageId(UUID.randomUUID().toString());
 		page.setChapterId(chapter.getChapterId());
 		page.setPageContent(savePath + File.separator + fileName);
 		page.setPageNumber(1);
+		
 		try {
 		    dao.inputChapter(chapter);
 		    dao.inputStoryPage(page);
